8a5ca91616c8f8a08e7735b6b3368f8611163c74
unknown <simlenghong@hotmail.com>
2017-05-20 20:22:31 +0800

    Inital Commit

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"time"
+	"github.com/simlenghong/yelp-fusion/yelp"
+)
+
+const (
+	mFolderLibs string = "libs"
+	mFolderHtml string = "html"
+)
+
+var (
+	mYelpAccessTokenTimeExpired time.Time 
+	mYelpAccessToken *yelp.AccessToken 
+)
+
+//Render Html pages
+func RenderPage(pResponseWriter http.ResponseWriter, pHtmlFileName string) {
+	log.Println("Processing " + pHtmlFileName + ".html... " )
+	lListHtmlFile := []string{ mFolderHtml + "/base.html", 
+				mFolderHtml + "/" + pHtmlFileName + ".html" }
+
+	lTemplatePtr, lErr := template.ParseFiles(lListHtmlFile...)
+	if lErr != nil {
+		log.Println("Error parsing files: ", lErr)
+	}
+
+	lErr = lTemplatePtr.Execute(pResponseWriter, nil)
+	if lErr != nil {
+		log.Println("Error executing template: ", lErr)
+	}
+}
+
+//Handler for processing files in libs folder
+func HandlerFilesLib(pResponseWriter http.ResponseWriter, pHttpRequest *http.Request) {
+	lPathLibFile := pHttpRequest.URL.Path[len("/" + mFolderLibs + "/"):]
+	
+	if len(lPathLibFile) != 0 {
+		lFile, lErr := http.Dir(mFolderLibs + "/").Open(lPathLibFile)
+		if lErr != nil {
+			log.Println("Error opening lib file: ", lErr)
+			http.NotFound(pResponseWriter, pHttpRequest)
+		} else {
+			lReadSeeker := io.ReadSeeker(lFile)
+			http.ServeContent(pResponseWriter, pHttpRequest, lPathLibFile, time.Now(), lReadSeeker)
+		}
+	}
+}
+
+//Handler for processing html/index.html
+func HandlerPageIndex(pResponseWriter http.ResponseWriter, pHttpRequest *http.Request) {
+	RenderPage(pResponseWriter, "index")
+}
+
+//Handler for searching business in yelp
+func HandlerSearchBusinessInYelp(pResponseWriter http.ResponseWriter, pRequest *http.Request) {
+	if IsYelpAccessTokenExpired() {
+		SetYelpAccessToken()
+	}
+
+	log.Println("Yelp access token expiring on " + mYelpAccessTokenTimeExpired.String()) 
+
+	var lYelpParamSearchBusiness yelp.ParamSearchBusiness 
+	lDecoder := json.NewDecoder(pRequest.Body)
+	lErr := lDecoder.Decode(&lYelpParamSearchBusiness)
+	if lErr != nil {
+	        log.Println("Error decoding Json string: ", lErr)
+	}
+	defer pRequest.Body.Close()
+
+	log.Println("Term: " + lYelpParamSearchBusiness.Term)
+	log.Println("Location: " + lYelpParamSearchBusiness.Location)
+
+	lYelpRespSearchBusinessMin, lErr := yelp.SearchBusinessMin(lYelpParamSearchBusiness, mYelpAccessToken)
+    	if lErr != nil {
+	        log.Println("Error searching businesses in Yelp: ", lErr)
+    	}
+	log.Println(lYelpRespSearchBusinessMin.Total)
+
+	pResponseWriter.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(pResponseWriter).Encode(lYelpRespSearchBusinessMin)
+}
+
+func IsYelpAccessTokenExpired() (bool) {
+	if time.Now().After(mYelpAccessTokenTimeExpired) {
+		log.Println("Yelp access token expired on " + mYelpAccessTokenTimeExpired.String()) 
+		return true
+	}
+	return false
+}
+
+func SetYelpAccessToken() {
+        log.Println("Retrieving Yelp access token...")
+	mYelpAccessTokenTimeExpired = time.Now()
+	mYelpAccessToken = new(yelp.AccessToken)
+	lErr := yelp.GetAccessToken(mYelpAccessToken)
+	if lErr != nil {
+		log.Println("Error retrieving Yelp access token: ", lErr)
+	}
+	lDuration := time.Duration(mYelpAccessToken.ExpiresIn*1000*1000*1000)
+	mYelpAccessTokenTimeExpired = mYelpAccessTokenTimeExpired.Add(lDuration)
+}
+
+//Main function
+func main(){
+	SetYelpAccessToken()
+
+        log.Println("Registering handlers...")
+	http.HandleFunc("/" + mFolderLibs + "/", HandlerFilesLib)
+	http.HandleFunc("/", HandlerPageIndex)
+	http.HandleFunc("/SearchBusiness", HandlerSearchBusinessInYelp)
+	
+        log.Println("Starting webserver...")
+	lErrListenAndServe := http.ListenAndServe(":8000", nil)
+	if lErrListenAndServe != nil {
+	        log.Println("Error starting webserver: ", lErrListenAndServe)
+	}
+}
---
+++ yelp/yelp.go
@@ -0,0 +1,181 @@
+package yelp
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+	mClientId = "YOUR_YELP_FUSION_CLIENT_ID"
+	mClientSecret = "YOUR_YELP_FUSION_CLIENT_SECRET"
+	mUrlAccessToken = "https://api.yelp.com/oauth2/token"
+	mUrlBusinessSearch = "https://api.yelp.com/v3/businesses/search"
+)
+
+type ParamSearchBusiness struct
+{
+	Term string `json:"term"`
+	Location string `json:"location"`
+}
+
+//For performance, return the required fields only
+type RespSearchBusinessMin struct {
+	Total      int64      `json:"total"`
+	Businesses []BusinessMin `json:"businesses"`
+}
+
+//For performance, return the required fields only
+type BusinessMin struct {
+	Rating      float32 `json:"rating"`
+	Name        string  `json:"name"`
+	ImgUrl      string  `json:"image_url"`
+	Location	struct {
+					City 		string `json:"city"`
+					Country 	string `json:"country"`
+					Address2 	string `json:"address2"`
+					Address3 	string `json:"address3"`
+					State 		string `json:"state"`
+					Address1 	string `json:"address1"`
+					ZipCode 	string `json:"zip_code"`
+	} `json:"location"`
+}
+
+type RespSearchBusiness struct {
+	Total      	int64      `json:"total"`
+	Businesses 	[]Business `json:"businesses"`
+	Region 		struct {
+				Center struct {
+						Latitude  float64 `json:"latitude"`
+						Longitude float64 `json:"longitude"`
+				} `json:"center"`
+			} `json:"region"`
+}
+
+type AccessToken struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int64  `json:"expires_in"`
+}
+
+type Business struct {
+	Rating 		float32 `json:"rating"`
+	Price 		string  `json:"price"`
+	Id 		string  `json:"id"`
+	IsClosed 	bool `json:"is_closed"`
+	Categories 	[]struct {
+					Alias string `json:"alias"`
+					Title string `json:"title"`
+	} `json:"categories"`
+	ReviewCount	int32 `json:"review_count"`
+	Name 		string  `json:"name"`
+	Url 		string  `json:"url"`
+	Coordinates 	struct {
+					Latitude  float64 `json:"latitude"`
+					Longitude float64 `json:"longitude"`
+	} `json:"coordinates"`
+	ImgUrl 		string  `json:"image_url"`
+	Location	struct {
+					City 		string `json:"city"`
+					Country 	string `json:"country"`
+					Address2 	string `json:"address2"`
+					Address3 	string `json:"address3"`
+					State 		string `json:"state"`
+					Address1 	string `json:"address1"`
+					ZipCode 	string `json:"zip_code"`
+	} `json:"location"`
+	Distance	float32 `json:"distance"`
+	Transactions	[]string `json:"transactions"`
+}
+
+//Get Yelp access token
+func GetAccessToken(pAccessToken *AccessToken) error {
+	lUrlValues := url.Values{}
+	lUrlValues.Add("grant_type", "client_credentials")
+	lUrlValues.Add("client_id", mClientId)
+	lUrlValues.Add("client_secret", mClientSecret)
+
+	lBufferUrlValues := bytes.NewBuffer([]byte(lUrlValues.Encode()))
+
+	lResponse, lErr := http.Post(mUrlAccessToken, "application/x-www-form-urlencoded", lBufferUrlValues)
+	if lErr != nil {
+		return lErr
+	}
+
+	defer lResponse.Body.Close()
+	lByteArr, lErr := ioutil.ReadAll(lResponse.Body)
+	if lErr != nil {
+		return lErr
+	}
+
+	lErr = json.Unmarshal(lByteArr, &pAccessToken)
+	if lErr != nil {
+		return lErr
+	}
+
+	return nil
+}
+
+//Yelp business search
+func SearchBusinessMin(pParamSearchBusiness ParamSearchBusiness, pAccessTokenPtr *AccessToken) (*RespSearchBusinessMin, error) { 
+	lUrlWithParam := mUrlBusinessSearch + "?" + "term=" + pParamSearchBusiness.Term + "&location=" + pParamSearchBusiness.Location
+
+	lRequest, lErr := http.NewRequest("GET", lUrlWithParam, nil)
+	if lErr != nil {
+		return nil, lErr
+	}
+
+	lRequest.Header.Set("Authorization", pAccessTokenPtr.TokenType + " " + pAccessTokenPtr.AccessToken)
+
+	lRefHttpClient := &http.Client{}
+	lResponse, lErr := lRefHttpClient.Do(lRequest)
+	if lErr != nil {
+		return nil, lErr
+	}
+
+	lByteArr, lErr := ioutil.ReadAll(lResponse.Body)
+	if lErr != nil {
+		return nil, lErr
+	}
+
+	var lRespSearchBusinessMin = new(RespSearchBusinessMin)
+	lErr = json.Unmarshal([]byte(lByteArr), &lRespSearchBusinessMin)
+	if lErr != nil {
+		return nil, lErr
+	}
+
+	return lRespSearchBusinessMin, nil
+}
+
+//Yelp business search
+func SearchBusiness(pParamSearchBusiness ParamSearchBusiness, pAccessTokenPtr *AccessToken) (*RespSearchBusiness, error) { 
+	lUrlWithParam := mUrlBusinessSearch + "?" + "term=" + pParamSearchBusiness.Term + "&location=" + pParamSearchBusiness.Location
+
+	lRequest, lErr := http.NewRequest("GET", lUrlWithParam, nil)
+	if lErr != nil {
+		return nil, lErr
+	}
+
+	lRequest.Header.Set("Authorization", pAccessTokenPtr.TokenType + " " + pAccessTokenPtr.AccessToken)
+
+	lRefHttpClient := &http.Client{}
+	lResponse, lErr := lRefHttpClient.Do(lRequest)
+	if lErr != nil {
+		return nil, lErr
+	}
+
+	lByteArr, lErr := ioutil.ReadAll(lResponse.Body)
+	if lErr != nil {
+		return nil, lErr
+	}
+
+	var lRespSearchBusiness = new(RespSearchBusiness)
+	lErr = json.Unmarshal([]byte(lByteArr), &lRespSearchBusiness)
+	if lErr != nil {
+		return nil, lErr
+	}
+
+	return lRespSearchBusiness, nil
+}
